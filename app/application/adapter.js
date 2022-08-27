59e4e89ce00bd093a6f17329269a3caee3a36d4d
Kamil Krywult <cutmaster12@gmail.com>
2022-08-27 11:01:45 +0200

    Add Bootstrap, pods and template

---
+++ app/application/adapter.js
@@ -0,0 +1,8 @@
+import EmberObject from '@ember/object';
+
+export default class Adapter extends EmberObject {
+  // findRecord(store, schema, id, snapshot) {
+  //   let { apiVersion } = snapshot.adapterOptions;
+  //   return this.adapterFor(`-api-${apiVersion}`).findRecord(store, schema, id, snapshot);
+  // }
+}
