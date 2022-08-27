8ceb8d76407d4a491e9d008a402391643d2a854f
Kamil Krywult <cutmaster12@gmail.com>
2022-08-27 16:26:34 +0200

    add date helper

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,5 +1,15 @@
 import { helper } from '@ember/component/helper';
-
-export default helper(function formatDate(positional /*, named*/) {
-  return positional;
-});
+//////////////////////////////// LICENSE///////////////////////////////////
+// you can use this helper without deleting this comment.
+// all rights reserved to Kamil Sobik
+function formatDate([date, type]) {
+  const shouldDisplayDate = type === 'long';
+  let formatedDate = date.toLocaleDateString();
+  if (shouldDisplayDate) {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    formatedDate = `${formatedDate} ${hours}:${minutes}`;
+  }
+  return formatedDate;
+}
+export default helper(formatDate);
